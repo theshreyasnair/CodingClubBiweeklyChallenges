3ca5b7a2eba64273a3daaceeefe2add1979e2ccb
Shreyas Nair <34798575+theshreyasnair@users.noreply.github.com>
2021-11-29 11:55:31 -0500

    Create BiweeklyChallenge3.py
    Added Biweekly Challenge 3.

---
+++ BiweeklyChallenge3.py
@@ -0,0 +1,15 @@
+print("""Mad Libs 1: 
+      Today I went to get ___ from the store. When I got home, I ____ it to make _____. However, because of this, my _____ started to _____! All in all, it was not a good day."""
+      )
+
+print("\nEnter words to fill in the blanks.")
+
+w1 = input("Noun: ")
+w2 = input("Verb: ")
+w3 = input("Noun: ")
+w4 = input("Noun: ")
+w5 = input("Verb: ")
+
+print(f"""Mad Libs 1: 
+      Today I went to get {w1} from the store. When I got home, I {w2} it to make {w3}. However, because of this, my {w4} started to {w5}! All in all, it was not a good day."""
+      )
